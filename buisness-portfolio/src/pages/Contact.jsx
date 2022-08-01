e2898e200e0308940dad13e94bf6783daa4908c3
Angel Santos <iiisantosjangel@gmail.com>
2022-08-01 16:24:20 -0700

    added to contact.jsx

--- buisness-portfolio/src/pages/Contact.jsx
+++ buisness-portfolio/src/pages/Contact.jsx
@@ -17,6 +17,71 @@ const Contact = () => {
 
     },4000);
 
-    // left off on emailjs
+    emailjs
+    .sendform(
+        "service_fu)e95l",
+        "template_gdjxp6g",
+        form.current,
+        "KfGVyjhX_IvddnHea"
+    )
+    .then(
+        (results) => {
+            console.log(result.text);
+        },
+        (error) => {
+            console.log(error.text);
+        }
+    );
+
+   e.target.reset();
     
-}
+    
+};
+return (
+    <section className={contact}>
+        <h2 className='text-white pt-4'>Contact Me</h2>
+
+        <div className="container contact_container">
+            <div className="contact_options">
+                <article className="contact_option">
+                    <AiOutlineMail className="contact_option-icon" />
+                    <h4>Email</h4>
+                    <a href="mailto: iiisantosjangel@gmail.com">Send Message</a>
+                </article>
+                <article className="cpntact_option">
+                    <BsMessenger className='contact_option-icon' />
+                    <h4>Messenger</h4>
+                    <a
+                    target='_blank'
+                    rel='noreferrer'
+                    href="htpps://m.me/angel.santos"
+                    >
+
+                    </a>
+                </article>
+                </div>
+                <form ref={form} onSubmit={sendEmail}>
+                    <input
+                    type="text"
+                    name="name"
+                    placeholder="Your Full Name"
+                    required
+                    />
+                    <input type="text" name='email' placeholder="Your Email" required />
+                    <textarea
+                    name='message'
+                    rows='5'
+                    placeholder='Your Message'
+                    required
+                    ></textarea>
+                    <button type='submit' className=" send btn btn-primary text-white">
+                        Send Message
+                    </button>
+                    <div className="feedback hide"><img src={flyingnembis} alt='' height='150'/>
+                    </div>
+                </form>
+                </div> 
+    </section>
+);
+
+export default Contact;
