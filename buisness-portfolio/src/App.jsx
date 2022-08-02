4d1848b6794b07de84c49f78473c95c3231faf32
Angel Santos <iiisantosjangel@gmail.com>
2022-08-01 19:37:32 -0700

    added App.jsx

--- buisness-portfolio/src/App.jsx
+++ buisness-portfolio/src/App.jsx
@@ -1,2 +1,23 @@
-// import {Routes, BrowserRouter as Router, Link, Route } from 'react-router-dom';
-// import Home from './pages/Home';
+ import {Routes, BrowserRouter as Router, Link, Route } from 'react-router-dom';
+ import Home from './pages/Home';
+ import Contact from './pages/Contact';
+ import About from './pages/About';
+ import Project from './pages/Portfolio';
+ import Global from './pages/Global';
+ import '.App.css';
+
+ function App(){
+    return (
+        <Router>
+            <Global/>
+            <Routes>
+                <Route path='/' element={<Home/>}/> 
+                <Route path='/about' element={<About/>}/>
+                <Route path='/project' element={<Project/>}/>
+                <Route path='/contact' element={<Contact/>}/>
+            </Routes>
+        </Router>
+    );
+ }
+
+ export default App;
